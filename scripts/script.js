60839254070fb6e8cf67729496046bfb5a512619
Iris <iris.van.willigen@gmail.com>
2024-10-09 14:16:49 +0200

    Extend script to work with focus

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,19 @@
-// JS
-
-/*
-buttons = document.querySelectorAll(".filters>li>button");
-buttons.forEach.addEventListener(mo)
-- Add event listener to buttons (hover)
-- get key of the button
-- document.querySelector(`img[data-filter="${e.keyCode}"]`);
-- focus, mouseover, mousedown
-event.target
-
-*/
-
 let buttons = document.querySelectorAll(".button-filter");
 
-buttons.forEach(button => button.addEventListener("mouseenter", (event) => {
-    let cat = button.innerHTML;
-    if (cat == "All Objects") return;
+buttons.forEach(button => button.addEventListener("mouseenter", function() { applyFilter(button) }));
+buttons.forEach(button => button.addEventListener("focus", function() { applyFilter(button) }));
+buttons.forEach(button => button.addEventListener("mouseleave", removeDim));
+buttons.forEach(button => button.addEventListener("blur", removeDim));
 
-    let objects = document.querySelectorAll(`img:not([data-filter="${cat}"])`)
+function applyFilter(button) {
+    let category = button.innerHTML;
+    if (category == "All Objects") return;
+    
+    let objects = document.querySelectorAll(`img:not([data-category="${category}"])`)
     objects.forEach(object => object.classList.add("dim"));
-}));
+}
 
-buttons.forEach(button => button.addEventListener("mouseleave", (event) => {
+function removeDim() {
     let objects = document.querySelectorAll(`img`)
     objects.forEach(object => object.classList.remove("dim"));
-}));
+}
