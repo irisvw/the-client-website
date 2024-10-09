b56b303cf7af8ba4a8c2fbf57754e806f0ec7fd2
Iris <iris.van.willigen@gmail.com>
2024-10-09 17:13:24 +0200

    Add script to generate images

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,14 +1,15 @@
+// FILTERS
 let buttons = document.querySelectorAll(".button-filter");
 
-buttons.forEach(button => button.addEventListener("mouseenter", function() { applyFilter(button) }));
-buttons.forEach(button => button.addEventListener("focus", function() { applyFilter(button) }));
+buttons.forEach(button => button.addEventListener("mouseenter", function () { applyFilter(button) }));
+buttons.forEach(button => button.addEventListener("focus", function () { applyFilter(button) }));
 buttons.forEach(button => button.addEventListener("mouseleave", removeDim));
 buttons.forEach(button => button.addEventListener("blur", removeDim));
 
 function applyFilter(button) {
     let category = button.innerHTML;
     if (category == "All Objects") return;
-    
+
     let objects = document.querySelectorAll(`img:not([data-category="${category}"])`)
     objects.forEach(object => object.classList.add("dim"));
 }
